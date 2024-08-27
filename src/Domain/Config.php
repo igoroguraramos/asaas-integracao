23ad41054b8dde6f1e2e3ee62c1d790d6eedd592
Igor Ogura Ramos <igoroguraramos@hotmail.com>
2024-08-27 09:07:22 -0300

    chore: environment define

--- src/Domain/Config.php
+++ src/Domain/Config.php
@@ -8,9 +8,21 @@ class Config {
     public $ssl = true;
     public $version = "v3";
     public $userAgent = "x";
+    public $production = true;
+    public $pathUrl;
 
     public function __construct(array $data = [])
     {
+        $production = $data["production"] ?? $this->production;
+        
+        $this->pathUrl = "/api/$this->version";
+
+        if($production)
+        {
+            $this->baseUri =  "https://api.asaas.com";
+            $this->pathUrl =  "/$this->version";
+        }
+
         foreach ($data as $key => $value) {
             if (property_exists($this, $key)) {
                 $this->$key = $value;
